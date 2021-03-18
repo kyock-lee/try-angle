95b774390dfee0d487ca30531bcc1baa56a7d3bc
kyock-lee <yakioni1031@gmailcom>
2021-03-18 17:19:37 +0900

    mealテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_18_060334) do
+ActiveRecord::Schema.define(version: 2021_03_18_081607) do
+
+  create_table "meals", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.date "date", null: false
+    t.text "description"
+    t.index ["user_id"], name: "index_meals_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -34,10 +43,11 @@ ActiveRecord::Schema.define(version: 2021_03_18_060334) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
-    t.integer "weight", null: false
-    t.date "record_date", null: false
+    t.float "weight"
+    t.date "date"
     t.index ["user_id"], name: "index_weights_on_user_id"
   end
 
+  add_foreign_key "meals", "users"
   add_foreign_key "weights", "users"
 end
