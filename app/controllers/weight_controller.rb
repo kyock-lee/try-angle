3b57bbda536e923cb08f833c541a61c85f9289ef
kyock-lee <yakioni1031@gmailcom>
2021-03-18 15:26:22 +0900

    weightsコントローラーの作成

---
+++ app/controllers/weight_controller.rb
@@ -0,0 +1,26 @@
+class WeightController < ApplicationController
+
+  def index
+    @weight = current_user.weights.includes(:user)
+  end
+
+  def new
+    @weight = Weight.new
+  end
+
+  def create
+    @weight = Weight.new(weight_params)
+    if @weight.save
+      redirect_to action: 'index'
+    else
+      render :new
+    end
+  end
+
+  private
+  def weight_params
+    params.require(:weight).permit(:weight, :date).merge(user_id: current_user.id)
+  end
+
+
+end
