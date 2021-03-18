866818eeb9d31b6da48f763b8b1f5dc132e5492e
kyock-lee <yakioni1031@gmailcom>
2021-03-18 14:58:42 +0900

    新規登録のバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :meals
+  has_many :weights
+  has_many :trainings
+  
+  with_options presence: true do
+    validates :nickname
+    validates :family_name
+    validates :family_name_kana
+    validates :last_name
+    validates :last_name_kana
+    validates :birthday
+  end
 end
