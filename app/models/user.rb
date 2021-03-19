add003c70e10ab259d00cd61b797fcc3ec73e89f
kyock-lee <yakioni1031@gmailcom>
2021-03-19 14:58:44 +0900

    ゲストログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,19 @@ class User < ApplicationRecord
     validates :last_name_kana
     validates :birthday
   end
+
+  def self.guest
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+    user.password = SecureRandom.urlsafe_base64
+    user.nickname = 'ゲスト'
+    user.family_name ='太朗'
+    user.family_name_kana ='タロウ'
+    user.last_name ='閲覧'
+    user.last_name_kana ='エツラン'
+    user.birthday = '2000-1-1'
+    end
+  end
+
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "users#index"
 
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
+
   resources :weights, only: [:index, :new, :create]
 
   resources :trainings, only: [:index, :new, :create] do
@@ -15,6 +19,5 @@ Rails.application.routes.draw do
       get 'search'
     end
   end
-
 
 end
