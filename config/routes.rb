68a0c01ae360c598096e84005588598519084dab
kyock-lee <yakioni1031@gmailcom>
2021-04-16 19:36:51 +0900

    ビューファイルの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: "users#index"
 
+  resources :user, only: [:index, :show]
+
   get '/users', to: redirect("/users/sign_up")
   
   devise_for :users, controllers: {
@@ -10,6 +12,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
+
+  resources :user, only: [:show]
 
   resources :weights, only: [:index, :new, :create, :edit, :update]
 
