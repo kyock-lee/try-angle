1b878c85b0759d44e59686b3e1746ce88419647d
kyock-lee <78146812+kyock-lee@users.noreply.github.com>
2021-04-25 12:26:20 +0900

    未実装部分の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @meal = Meal.find(params[:id])
     @weight = Weight.find_by(id: params[:format])
-    # @meals = current_user.meals.includes(:user).order('created_at DESC').limit(9)
-    # @training = current_user.trainings.includes(:user).order('created_at DESC').limit(30)
-    # @weight = current_user.weights.includes(:user)
   end
 
   private
